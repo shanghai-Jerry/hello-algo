016c0a5cec7ff8edb1b48486a3719577ff36bdd3
youchaojiang <icbigdata@gmail.com>
2023-07-04 20:46:18 +0800

    add check graph utils and not ready to merge to main

--- codes/go/chapter_graph/graph_adjacency_list_test.go
+++ codes/go/chapter_graph/graph_adjacency_list_test.go
@@ -11,7 +11,7 @@ import (
 	. "github.com/krahets/hello-algo/pkg"
 )
 
-func TestGraphAdjList(t *testing.T) {
+func TestGraphAdjListLocal(t *testing.T) {
 	/* 初始化无向图 */
 	v := ValsToVets([]int{1, 3, 2, 5, 4})
 	edges := [][]Vertex{{v[0], v[1]}, {v[0], v[3]}, {v[1], v[2]}, {v[2], v[3]}, {v[2], v[4]}, {v[3], v[4]}}
